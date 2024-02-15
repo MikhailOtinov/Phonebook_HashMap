8b8aabf73872b8c2e78bb43968e4f76de26ca7c6
Mikhail <avt.srt@yandex.ru>
2024-02-15 22:27:22 +0500

    TaskBook2

--- src/main/java/TaskBookNext.java
+++ src/main/java/TaskBookNext.java
@@ -4,29 +4,29 @@ import java.util.Map;
 
 public class TaskBookNext {
     // Метод добавляет номера
-    public static void addNumber(String key, int value, Map<String, ArrayList<Integer>> map) {
+    public static void addNumber(String key, long value, Map<String, ArrayList<Long>> map) {
         if (map.containsKey(key)) {
             map.get(key).add(value);
         } else {
-            ArrayList<Integer> list = new ArrayList<>();
+            ArrayList<Long> list = new ArrayList<>();
             list.add(value);
             map.put(key, list);
         }
     }
     // Метод для печати контактов
-    public static void printBook(Map<String, ArrayList<Integer>> map) {
+    public static void printBook(Map<String, ArrayList<Long>> map) {
         for (var item : map.entrySet()) {
             String phones = "";
-            for (int el : item.getValue()) {
+            for (long el : item.getValue()) {
                 phones = phones + el + ", ";
             }
             System.out.printf("%s: %s \n", item.getKey(), phones);
         }
     }
     public static void main(String[] args) {
-        Map<String, ArrayList<Integer>> bookPhone = new HashMap<>();
-        addNumber("Вася", 899912563, bookPhone);
-        addNumber("Вася", 880022891, bookPhone);
+        Map<String, ArrayList<Long>> bookPhone = new HashMap<>();
+        addNumber("Вася", 89925777799L, bookPhone);
+        addNumber("Вася", 88005522891L, bookPhone);
         addNumber("Петя", 87451872, bookPhone);
         addNumber("Юля", 860852731, bookPhone);
         addNumber("Коля", 875412688, bookPhone);
